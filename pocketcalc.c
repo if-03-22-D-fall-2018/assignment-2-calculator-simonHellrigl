19c38e37c82a210bc0dc2ca129f7f0e5f6795068
simonHellrigl <43413178+simonHellrigl@users.noreply.github.com>
2018-10-02 16:23:52 +0200

    Add files via upload
    Zwischendurch Speichern.

--- pocketcalc.c
+++ pocketcalc.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-void GetOperands (double* firstOperand, double* secondOperand) {
-  printf("Please enter the first operand: ");
-  scanf("%lf", firstOperand);
-  printf("Please enter the second operand: ");
-  scanf("%lf", secondOperand);
-}
+void GetOperands(double *firstOperand, double* secondOperand);
+void CalculateResults(double firstOperand, double secondOperand, int type_Of_Operation, bool breakCondition);
 
 int  main(int argc, char const *argv[]) {
 
@@ -14,35 +10,52 @@ int  main(int argc, char const *argv[]) {
   bool breakCondition=false;
   double firstOperand;
   double secondOperand;
-  double result;
 
-  printf("Pocketcalculator\n");
-  printf("================\n");
-  printf("Select Add(1), Subtract(2), Multiply(3), Divide(4) or stop(-1): ");
-  scanf("%d", &type_Of_Operation);
 
-  GetOperands(&firstOperand, &secondOperand);
-  do {
-    if (type_Of_Operation == 1) {
+  printf("Pocketcalculator!\n");
+  printf("=================\n");
+  while (breakCondition == false) {
+    printf("Select Add(1), Subtract(2), Multiply(3), Divide(4) or stop(-1): ");
+    scanf("%d", &type_Of_Operation);
+    GetOperands(&firstOperand, &secondOperand);
+    CalculateResults(firstOperand, secondOperand, type_Of_Operation, breakCondition);
 
-      result = firstOperand + secondOperand;
-    }
-    else if (type_Of_Operation ==  2) {
-      result = firstOperand - secondOperand;
-    }
-    else if (type_Of_Operation == 3) {
-      result = firstOperand * secondOperand;
-    }
-    else if (type_Of_Operation == 4) {
-      result = firstOperand / secondOperand;
-    }
-    else if (type_Of_Operation == -1) {
-      breakCondition = true;
-    }
-    else{
-      printf("Input not allowed, please try again\n");
-    }
-  } while(breakCondition != true);
+  }
+
 
   return 0;
+}
+
+void GetOperands (double* firstOperand, double* secondOperand) {
+  printf("Please enter the first operand: ");
+  scanf("%lf", firstOperand);
+  printf("Please enter the second operand: ");
+  scanf("%lf", secondOperand);
+}
+
+void CalculateResults(double firstOperand, double secondOperand, int type_Of_Operation, bool breakCondition){
+
+  double result;
+  if (type_Of_Operation == 1) {
+
+    result = firstOperand + secondOperand;
+  }
+  else if (type_Of_Operation ==  2) {
+    result = firstOperand - secondOperand;
+  }
+  else if (type_Of_Operation == 3) {
+    result = firstOperand * secondOperand;
+  }
+  else if (type_Of_Operation == 4) {
+    result = firstOperand / secondOperand;
+  }
+  else if (type_Of_Operation == -1) {
+     breakCondition = true;
+  }
+  else {
+    printf("Input not allowed, please try again\n");
+  }
+  printf("\nResult: %lf\n\n", result);
+
+
 }
